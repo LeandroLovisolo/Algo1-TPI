296e69574b63c607a64c51aa7859242f7d60bd8f
Cande <canduh_27@hotmail.com>
2012-06-08 18:04:34 -0300

    Agregue primeros ej jjoo

--- jjoo.cpp
+++ jjoo.cpp
@@ -3,3 +3,36 @@
 #include "lista.h"
 #include "jjoo.h"
 
+JJOO::JJOO () {
+    _jornadaActual = 1;
+}
+JJOO::JJOO (const int anio, const Lista<Atleta>& atletas, const Lista<Lista<Competencia> >& competenciasPorDia){
+    _anio = anio;
+    _atletas = atletas;
+    _jornadaActual = 1;
+    _competenciasPorDia = competenciasPorDia;
+}
+int JJOO::anio() const{
+    return _anio;
+}
+Lista<Atleta> JJOO::atletas() const {
+    return _atletas;
+}
+int JJOO::cantDias() const {
+    return _competenciasPorDia.longitud ();
+}
+int JJOO::jornadaActual() const {
+    return _jornadaActual;
+}
+Lista<Competencia> JJOO::cronograma(const int dia) const {
+    return _competenciasPorDia.iesimo(dia);
+}
+Lista<Competencia> JJOO::competencias() const {
+    int i = 0;
+    Lista<Competencia> competencias;
+        while (i<cantDias()) {
+            i=i+1;
+            competencias.concatenar(_competenciasPorDia.iesimo(i));
+            }
+    return competencias;
+}
