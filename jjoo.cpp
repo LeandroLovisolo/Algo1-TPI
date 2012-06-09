7f2c013062a0f29e4dee75f43d5143355bfdfd5b
Lautaro Petaccio <lausuper@gmail.com>
2012-06-09 15:37:23 -0300

    Arreglado pequeño bug en dePaseo y agregado liuSong

--- jjoo.cpp
+++ jjoo.cpp
@@ -39,9 +39,9 @@ Lista<Competencia> JJOO::competenciasFinalizadasConOroEnPodio() const {
 	int i = 0;
 	Lista<Competencia> comp;
         while (i < competencias().longitud())   {
-            if ((competencias().iesimo(i)).finalizada() && ((competencias().iesimo(i)).ranking()).longitud() > 0 )
-                {comp.agregar(competencias().iesimo(i));
-                }
+            if ((competencias().iesimo(i)).finalizada() && ((competencias().iesimo(i)).ranking()).longitud() > 0 ) {
+            	comp.agregar(competencias().iesimo(i));
+            }
             i++;
         }
 	return comp;
@@ -50,8 +50,10 @@ Lista<Competencia> JJOO::competenciasFinalizadasConOroEnPodio() const {
 Lista<Atleta> JJOO::dePaseo() const {
     int i=0;
     Lista<Atleta> atletas;
-        while (i < _atletas.longitud()) {
-            if (participantesJJOO(competencias()).pertenece(_atletas.iesimo(i))) {
+        while (i< _atletas.longitud()) {
+        	//Si no alguno de los que esta en la lista de atletas del JJOO no pertenece a la lista de participantes lo agrega
+        	//Faltaba la negacion(!)
+            if (!participantesJJOO(competencias()).pertenece(_atletas.iesimo(i))) {
                 atletas.agregar(_atletas.iesimo(i));
             }
             i++;
@@ -70,8 +72,80 @@ int JJOO::boicotPorDisciplina(const Categoria cat, const Pais p) {
 Lista<Atleta> JJOO::losMasFracasados(const Pais p) const {
 	return Lista<Atleta>();
 }
-void JJOO::liuSong(const Atleta& a, const Pais p) {
 
+//Por que se crea toda la lista otra vez? porque no hay funciones publicas que permitan modificar atletas en competencias, incluso
+//iesimo devuelve una constante, no queda otra
+void JJOO::liuSong(const Atleta& a, const Pais p) {
+	Atleta atletaNacionalizado(a.nombre(), a.sexo(), a.anioNacimiento(), p, a.ciaNumber());
+	int i = 0;
+	while(i<a.deportes().longitud()) {
+		atletaNacionalizado.entrenarNuevoDeporte(a.deportes().iesimo(i), a.capacidad(a.deportes().iesimo(i)));
+		i++;
+	}
+
+	atletaNacionalizado.mostrar(cout);
+
+	//Saco el atleta y lo agrego nacionalizado en atletas;
+	_atletas.sacar(a);
+	_atletas.agregarAtras(atletaNacionalizado);
+
+	i=0;
+	int j = 0;
+	//Lista que va a reemplazar a la vieja lista de listas competencias
+	Lista<Lista<Competencia> > nuevaCompetenciasPorDia;
+	while(i<_competenciasPorDia.longitud()) {
+		//Lista de competencias que va a reemplazar a la anterior en ese dia
+		Lista<Competencia> nuevaCompetenciasEnDia;
+		j = 0;
+		while(j<_competenciasPorDia.iesimo(i).longitud()) {
+			//Me fijo si esa competencia tiene al atleta, si no, la dejo como estaba
+			if(_competenciasPorDia.iesimo(i).iesimo(j).participantes().pertenece(a)) {
+				Competencia viejaCompe = _competenciasPorDia.iesimo(i).iesimo(j);
+				//Creo la nueva lista de participantes
+				Lista<Atleta> nuevosParticipantes = viejaCompe.participantes();
+				nuevosParticipantes.sacar(a);
+				nuevosParticipantes.agregarAtras(atletaNacionalizado);
+				//
+				//Guardo la categoria
+				Deporte dep = _competenciasPorDia.iesimo(i).iesimo(j).categoria().first;
+				Sexo sex = _competenciasPorDia.iesimo(i).iesimo(j).categoria().second;
+				//
+				//Creo lo que va a ser la nueva competencia con el atleta cambiado
+				//
+				Competencia nuevaCompe(dep, sex, nuevosParticipantes);
+
+				//Si esta finalizada, asigno el ranking y el doping
+				if(viejaCompe.finalizada()) {
+					int h = 0;
+					Lista<int> ranking;
+					Lista<pair<int, bool> > control;
+					while(h<viejaCompe.ranking().longitud()) {
+						ranking.agregarAtras(_competenciasPorDia.iesimo(i).iesimo(j).ranking().iesimo(h).ciaNumber());
+						h++;
+					}
+					h = 0;
+					while(h<viejaCompe.lesTocoControlAntidoping().longitud()) {
+						bool leToco = viejaCompe.leDioPositivo(viejaCompe.lesTocoControlAntidoping().iesimo(h));
+						int suCiaNumber = viejaCompe.lesTocoControlAntidoping().iesimo(h).ciaNumber();
+						pair<int, bool> par = make_pair(suCiaNumber, leToco);
+						control.agregarAtras(par);
+						h++;
+					}
+					//Finalizo
+					nuevaCompe.finalizar(ranking, control);
+				}
+				//Agrego a lo que va a ser mi nueva lista de competencias en ese dia
+				nuevaCompetenciasEnDia.agregarAtras(nuevaCompe);
+			}
+			else {
+				nuevaCompetenciasEnDia.agregarAtras(_competenciasPorDia.iesimo(i).iesimo(j));
+			}
+			j++;
+		}
+		i++;
+		nuevaCompetenciasPorDia.agregarAtras(nuevaCompetenciasEnDia);
+	}
+	_competenciasPorDia = nuevaCompetenciasPorDia;
 }
 Atleta JJOO::stevenBradbury() const {
 	return Atleta();
