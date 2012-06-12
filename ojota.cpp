1a9e5448692f53ec25b0e05b9879c2757528bc29
Lautaro Petaccio <lausuper@gmail.com>
2012-06-12 11:00:33 -0300

    Implementado atleta prodigio lo mas simple posible

--- ojota.cpp
+++ ojota.cpp
@@ -4,8 +4,18 @@
 using namespace std;
 
 Atleta atletaProdigio(const JJOO & j) {
-	Atleta atle;
-	return atle;
+	Atleta atletaMinimaEdad = j.competenciasFinalizadasConOroEnPodio().iesimo(0).ranking().iesimo(0);
+	int i=1;
+	int cantidadCompetenciasConOro = j.competenciasFinalizadasConOroEnPodio().longitud();
+	while(i<cantidadCompetenciasConOro) {
+		int anioAtleta = j.competenciasFinalizadasConOroEnPodio().iesimo(i).ranking().iesimo(0).anioNacimiento();
+		int anioAtletaEdadMinima = atletaMinimaEdad.anioNacimiento();
+		if(anioAtleta <= anioAtletaEdadMinima) {
+			atletaMinimaEdad = j.competenciasFinalizadasConOroEnPodio().iesimo(i).ranking().iesimo(0);
+		}
+		i++;
+	}
+	return atletaMinimaEdad;
 }
 
 int main(){
